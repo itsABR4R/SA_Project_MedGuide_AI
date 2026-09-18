7aceebe5f7c51aa314d46aa2444b77253f2c9e21
nafizabdullah09 <nafizabdullah09@gmail.com>
2026-09-18 13:50:19 +0600

    .

--- server/src/utils/app-error.js
+++
@@ -1,8 +0,0 @@
-export class AppError extends Error {
-  constructor(status, code, message, options = {}) {
-    super(message, options);
-    this.name = 'AppError';
-    this.status = status;
-    this.code = code;
-  }
-}
